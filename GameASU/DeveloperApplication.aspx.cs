6f2eae13adaad57677087af5c886b6a3647187bd
Chris Hamilton <cohamilt@gmail.com>
2014-09-27 15:09:28 -0700

    Setup for Developer Sign Up Sheet

---
+++ GameASU/DeveloperApplication.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GameASU
+{
+    public partial class DeveloperApplication : System.Web.UI.Page
+    {
+
+        
+
+        protected void Page_Load()
+        {
+            
+        }
+
+        protected void CheckDevRole_Click(object sender, EventArgs e)
+        {
+            if (!acceptTerms.Checked)
+            {
+                TermsLabel.ForeColor = System.Drawing.Color.Red;
+                TermsLabel.Text = "Please accept the terms to become a developer.";
+            }
+            else
+            {
+                //dole out developer role!
+            }
+        }
+
+      
+    }
+}
